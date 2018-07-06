6c8b506e9af7498e2766bc5043499808c76feef8
brapls <brianderg@gmail.com>
2018-07-06 10:57:45 +0200

    Test.java created

---
+++ kinobuchsystem/src/test.java
@@ -0,0 +1,30 @@
+import model.Besucher;
+import model.Film;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by brian on 06.07.2018.
+ */
+public class test {
+    @Test
+    public void testBesucher(){
+        Besucher besucher = new Besucher("Telefonnummer", "Vorname", "Nachname");
+        Assertions.assertEquals(besucher.getTelefonnummer(), "Telefonnummer");
+        Assertions.assertEquals(besucher.getVorname(), "Vorname");
+        Assertions.assertEquals(besucher.getNachname(), "Nachname");
+        besucher.setNachname("Nachname2");
+        besucher.setVorname("Vorname2");
+        besucher.setTelefonnummer("Telefonnummer2");
+        Assertions.assertEquals(besucher.getTelefonnummer(), "Telefonnummer2");
+        Assertions.assertEquals(besucher.getVorname(), "Vorname2");
+        Assertions.assertEquals(besucher.getNachname(), "Nachname2");
+    }
+    @Test
+    public void i-,(){
+    Film film = new Film(1, "Titel", "Dauer");
+        Assertions.assertEquals(film.getId().toString(), "1");
+        Assertions.assertEquals(film.getTitel(), "Titel");
+        Assertions.assertEquals(film.getDauer(), "Dauer");
+    }
+}
