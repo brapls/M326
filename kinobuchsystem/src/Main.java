28b0a1fe01a5ba1ffb78ae5270ba15b597641fb3
Rainer Kaufmann <CasalEstrelar@Rainers-MBP.tbz.local>
2018-07-06 08:52:12 +0200

    main update 6 juli

--- kinobuchsystem/src/Main.java
+++ kinobuchsystem/src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
+      Sammlung sammlung = new Sammlung();
+        System.out.println("1. Filme abrufen");
+        System.out.println("2. Reservieren");
+        System.out.println("3. Reservierung löschen");
+        String value = readLineString();
+        Integer val;
+        try{
+          Integer val = (Integer) value;
+        }catch (Exception e) {
+          Integer val = 0;
+        }
 
+        switch(val){
+          case 1:
+            ArrayList<Vorstellung> vorstellungen = sammlung.getVorstellungen();
+            break;
+          case 2:
+            break;
+        }
+    }
+
+    private static String readLineString(){
+      Strin input = null;
+      Scanner scanner = new Scanner(System.in);
+      input = scanner.nextLine();
+      return input;
     }
 }
